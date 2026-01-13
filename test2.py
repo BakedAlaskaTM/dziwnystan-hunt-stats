fbf4aa37da3503271213bbcae13de69ef34330c1
Devin Shen <ds21616@my.westlake.school.nz>
2026-01-14 09:38:23 +1300

    Updated Data 14/01/2026

--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
 from functions import *
 
-update_tmx_players()
+ml_info = read_json(f"{DATA_FILE_PATH}ml_info.json")
+
+for key, value in ml_info.items():
+    ml_info[key] = {
+        "Login": key,
+        "TMX": value
+    }
+
+write_json(f"{DATA_FILE_PATH}ml_info.json", ml_info)
